4f16fca49127a587654c1a7f152b045e9a4b9b60
jyotimishra05 <jyotihra28@gmail.com>
2025-08-30 20:51:52 +0530

    Add mmultipkication

---
+++ Multiplication.java
@@ -0,0 +1,11 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int result = a * b;
+        System.out.println("The multiplication of  " + a + " and " + b + " is: " + result);
+    }
+
+}
+
